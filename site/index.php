c2ba7e7808b7064592da0382414e7137402666e4
Saabbass <178414@novacollege.nl>
2023-02-02 21:22:47 +0100

    opdracht 1 new

--- site/index.php
+++ site/index.php
@@ -1,4 +1,41 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-// phpinfo();
-echo "<h1>I am a big fan of the Simpsons</h1>";
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>opdracht 12</title>
+</head>
+
+<body>
+    <h1>
+        <?php
+
+        echo "I am a big fan of the simpsons"
+
+        ?>
+    </h1>
+
+    <?php 
+
+    $willekeurigeVariable = 3;
+    $willekeurigeVariable = true;
+    $willekeurigeVariable = [true, "yes", 4, "Sebas"];
+
+    $mijnGegevens = [
+        "voornaam" => "Sebas", 
+        "achternaam" => "Tenk", 
+        "leeftijd" => 21];
+
+        echo $mijnGegevens["leeftijd"];
+    ?>
+
+
+</body>
+
+</html>
+
+
+<!-- phpinfo();
+     echo "<h1>I am a big fan of the Simpsons</h1>"; -->
